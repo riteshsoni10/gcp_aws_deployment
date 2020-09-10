8316e8742f401050ef89a6bc22b9ebbcce30f63a
riteshsoni10 <riteshk.296@gmail.com>
2020-09-11 04:38:49 +0530

    added kuberenetes provider

--- automation-scripts/main.tf
+++ automation-scripts/main.tf
@@ -2,6 +2,7 @@ terraform {
     required_version = ">= 0.12"
 }
 
+
 provider "aws" {
     region = var.aws_region_name
     profile = var.aws_user_profile
@@ -15,10 +16,25 @@ provider "google" {
     version = "~> 3.38"
 }
 
+provider "kubernetes" {
+    version = "~> 1.13"
+    load_config_file = false
+    host = "https://${module.gcp_cloud.gke_endpoint}"
+    cluster_ca_certificate = base64decode(module.gcp_cloud.gke_ca_certificate)
+    token = module.gcp_cloud.gke_config.access_token
+}
 
 module "gcp_cloud" {
     source = "./modules/gcp"
     subnet_cidr = var.gcp_subnet_cidr
     network_name = var.gcp_network_name
-
+    cluster_name = var.gcp_cluster_name
+    cluster_zone = var.gcp_cluster_zone
+    load_balancing_state = var.gcp_load_balancing_state
+    node_count = var.gcp_node_count
+    node_disk_size  = var.gcp_node_disk_size
+    node_preemptible_state = var.gcp_node_preemptible_state
+    node_machine_type = var.gcp_node_machine_type
+    pod_scaling_state = var.gcp_pod_scaling_state
+    node_pool_name = var.gcp_node_pool_name
 }
