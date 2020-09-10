3e0241a608a516e5c62df1734cfea7534a5135b0
riteshsoni10 <riteshk.296@gmail.com>
2020-09-10 23:06:53 +0530

    Added gcp module

--- automation-scripts/main.tf
+++ automation-scripts/main.tf
@@ -11,5 +11,14 @@ provider "aws" {
 provider "google" {
     region      = var.gcp_region_name
     credentials = file(var.gcp_credentials_file_name)
+    project     = var.gcp_project_id
     version = "~> 3.38"
+}
+
+
+module "gcp_cloud" {
+    source = "./modules/gcp"
+    subnet_cidr = var.gcp_subnet_cidr
+    network_name = var.gcp_network_name
+
 }
