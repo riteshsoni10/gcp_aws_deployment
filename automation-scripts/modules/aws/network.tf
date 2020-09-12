79e2a23227905a03781d58168857900806882a02
riteshsoni10 <riteshk.296@gmail.com>
2020-09-13 03:59:41 +0530

    aws_network setup resources added

---
+++ automation-scripts/modules/aws/network.tf
@@ -0,0 +1,88 @@
+## Internet Gateway
+resource "aws_internet_gateway" "application_igw" {
+    vpc_id = aws_vpc.backend_network.id
+    tags = {
+        Name = "Backend-IGW"
+    }
+    depends_on = [
+        aws_vpc.backend_vpc
+    ]
+}
+
+
+## Nat Gateway
+
+resource "aws_nat_gateway" "nat_gw" {
+    allocation_id = aws_eip.nat_public_ip.id
+    subnet_id     = aws_subnet.public_subnets.0.id
+
+    depends_on = [
+        aws_eip.nat_public_ip,
+        aws_subnet.public_subnets,
+    ]
+}
+
+# Public Route Table
+resource "aws_route_table" "public_route_table" {
+    vpc_id = aws_vpc.backend_network.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.application_igw.id
+    }
+    
+    tags = {
+        Name = "public_route_table"
+    }
+
+    depends_on = [
+        aws_internet_gateway.application_igw,
+        aws_vpc.backend_network,
+    ]
+}
+
+## Private Route Table
+
+resource "aws_route_table" "private_route_table" {
+    vpc_id = aws_vpc.backend_network.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_nat_gateway.nat_gw.id
+    }
+
+    tags = {
+        Name = "private_route_table"
+    }
+
+    depends_on = [
+        aws_vpc.backend_network,
+        aws_nat_gateway.nat_gw
+    ]
+}
+
+## Public Route Table Association => Public-Subnets
+resource "aws_route_table_association" "public_route_table_association" {
+    count          = length(data.aws_availability_zones.availability_zones.names)
+    subnet_id      = element(aws_subnet.public_subnets.*.id,count.index)
+    route_table_id = aws_route_table.public_route_table.id
+
+    depends_on     = [
+        aws_subnet.public_subnets,
+        aws_route_table.public_route_table,
+    ]
+}
+
+## Private Route Table Association => Private subnets
+
+resource "aws_route_table_association" "private_route_table_association" {
+    count      = length(data.availability_zones.availability_zones.names)
+    subnet_id  = element(aws_subnet.private_subnets.*.id,count.index)
+    route_table_id = aws_route_table.private_route_table.id
+
+    depends_on = [
+        aws_subnet.private_subnets,
+        aws_route_table.private_route_table,
+    ]
+}
+
+
+
---
+++ automation-scripts/modules/aws/vpc.tf
@@ -0,0 +1,56 @@
+## AWS Availability Zones
+
+data "aws_availability_zones" "available_zones" {
+    state = "available"
+}
+
+## Backend Database Network
+resource "aws_vpc" "backend_network" {
+    cidr_block = var.vpc_cidr_block
+    tags       = {
+            Name = "Backend-Network"
+    } 
+}
+
+## Subnets
+
+resource "aws_subnet" "public_subnets" {
+    count                   = length(data.aws_availability_zones.availability_zones.names)
+    cidr_block              = cidrsubnet(var.vpc_cidr_block,8,count.index)
+    vpc_id                  = aws_vpc.backend_nework.id
+    availability_zone       = element(data.aws_availability_zones.availability_zones.names,count.index)
+    map_public_ip_on_launch = true
+    tags = {
+        Name  = "Public Subnet - ${element(data.aws_availability_zones.availability_zones.names,count.index)}"
+    }
+
+    depends_on =[
+        aws_vpc.backend_network
+    ]
+
+}
+
+resource "aws_subnet" "private_subnets" {
+    count  = lenght(data.aws_availability_zones.availability_zones.names)
+    cidr_block  = cidrblock(var.vpc_cidr_block,8,"${10+count.index}")
+    vpc_id      = aws_vpc.backend_network.id
+    availability_zone  = element(data.aws_availability_zones.availability_zones.names,count.index)
+    tags  = {
+        Name  = "Private Subnet - ${element(data.aws_availability_zones.availability_zones.names,count.index)}"
+    }
+
+    depends_on = [
+        aws_vpc.backend_network
+    ]
+}
+
+## Public IP for NAT Gateway
+
+resource "aws_eip" "nat_public_ip"{
+    vpc    = true
+
+    tags = {
+        Name = "NAt -GW Public IP"
+    }
+}
+
