9a2c4e3b96649729226f1937108aeb95aa39341c
riteshsoni10 <riteshk.296@gmail.com>
2020-09-13 06:05:48 +0530

    output variable gcp network_id

--- automation-scripts/modules/gcp/output.tf
+++ automation-scripts/modules/gcp/output.tf
@@ -9,3 +9,7 @@ output "gke_ca_certificate" {
 output "gke_config" {
     value = data.google_client_config.current_config
 }
+
+output "network_id" {
+    value = google_compute_network.app_network.id
+}
