dfb71cb97b2e41bc6fd37c85aabd32317e8ed249
riteshsoni10 <riteshk.296@gmail.com>
2020-09-13 06:03:35 +0530

    output variables from aws module

---
+++ automation-scripts/modules/aws/output.tf
@@ -0,0 +1,11 @@
+output "vpc_id" {
+    value = aws_vpc.backend_network.id
+}
+
+output "public_route_table_id" {
+    value = aws_route_table.public_route_table.id
+}
+
+output "private_route_table_id" {
+    value = aws_route_table.private_route_table.id
+}
