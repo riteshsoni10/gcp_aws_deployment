13d3bc201a5c922c76fff2b259fbfb1ebe930b96
Ritesh Kumar <riteshk.296@gmail.com>
2020-09-17 02:29:09 +0530

    Update vars.tf

--- automation-scripts/modules/kubernetes/vars.tf
+++ automation-scripts/modules/kubernetes/vars.tf
@@ -2,3 +2,28 @@ variable "mongo_db_port" {
   type = string
   description = " Database Server Port"
 }
+
+variable "db_root_username" {
+  type = string
+  description = "Database Server Admin UserName"
+}
+
+variable "db_root_password" {
+  type = string
+  description = " Database Admin Password"
+}
+
+variable "db_app_username" {
+  type = string
+  description = "Database Server Application UserName"
+}
+
+variable "db_app_password" {
+  type = string
+  description = " Database Application User Password"
+}
+
+variable "db_database_name" {
+  type = string
+  description = "Application Database Name"
+}
