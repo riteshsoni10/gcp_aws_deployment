641535564fac14bc43c577aac1858804519f5738
Ritesh Kumar <riteshk.296@gmail.com>
2020-09-17 03:16:39 +0530

    Update secrets.tf

--- automation-scripts/modules/kubernetes/secrets.tf
+++ automation-scripts/modules/kubernetes/secrets.tf
@@ -4,8 +4,6 @@ resource "kubernetes_secret" "db_secret" {
 	}
 
 	data = {
-		root_username = var.db_root_username
-		root_password = var.db_root_password
 		username      = var.db_app_username
 		password      = var.db_app_password 
 		database      = var.db_database_name
