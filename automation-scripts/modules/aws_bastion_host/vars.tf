5383a318e8bd7f5ac762db5a5a065967ad72f568
Ritesh Kumar <riteshk.296@gmail.com>
2020-09-17 02:00:38 +0530

    Added mongo_server role variables

--- automation-scripts/modules/aws_bastion_host/vars.tf
+++ automation-scripts/modules/aws_bastion_host/vars.tf
@@ -3,13 +3,10 @@ variable "vpc_id" {
 	description = " AWS VPC Id "
 }
 
-
-
 variable "key_name" {
 	type = string
 	description = "Instance Key Name"
 }
-
 
 variable "public_subnet_id" {
 	type = string
@@ -26,6 +23,8 @@ variable "bastion_ami_id" {
 	description = "AMI for Database Server"
 }
 
+# CLI Command to AMI Id in region
+# aws ec2 describe-images --owners 309956199498 --query 'sort_by(Images, &CreationDate)[*].[CreationDate,Name,ImageId]' --filters "Name=name,Values=RHEL-8*" --region ap-southeast-1 --output table --profile aws_terraform_user
 
 variable "connection_user" {
     type = string
@@ -53,5 +52,42 @@ variable "db_instance_key_name" {
 	description = "Database Server Key name"
 }
 
-# CLI Command to AMI Id in region
-# aws ec2 describe-images --owners 309956199498 --query 'sort_by(Images, &CreationDate)[*].[CreationDate,Name,ImageId]' --filters "Name=name,Values=RHEL-8*" --region ap-southeast-1 --output table --profile aws_terraform_user
+
+## Mongo Database Server Configuration variables
+
+variable "mongo_db_root_username" {
+    type = string
+    description = "The login user for SSH connection"
+}
+
+variable "mongo_db_root_password" {
+    type= string
+    description = "Connection type for remote login"
+}
+
+variable "mongo_db_server_port" {
+	type = string
+	description = "Database Server Private IP"
+}
+
+variable "mongo_db_data_path" {
+	type = string
+	description = "Database Login User"
+
+}
+
+variable "mongo_db_application_username" {
+	type  = string
+	description = "Database Server Key name"
+}
+
+variable "mongo_db_application_user_password" {
+	type = string
+	description = "Database Login User"
+
+}
+
+variable "mongo_db_application_db_name" {
+	type  = string
+	description = "Database Server Key name"
+}
