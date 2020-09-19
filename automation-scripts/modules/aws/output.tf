e49a5591e1ac0dfaef1e19a66ccb33ebc6a37d40
riteshsoni10 <riteshk.296@gmail.com>
2020-09-19 08:11:38 +0530

    Added AWS module Outputs

--- automation-scripts/modules/aws/output.tf
+++ automation-scripts/modules/aws/output.tf
@@ -10,11 +10,26 @@ output "private_route_table_id" {
     value = aws_route_table.private_route_table.id
 }
 
-output "private_subnet_id" {
-    value = aws_subnet.private_subnets.0.id
+output "public_subnet_cidrs" {
+	value = aws_subnet.public_subnets.*.cidr_block 
 }
 
-output "public_subnet_id" {
-    value = aws_subnet.public_subnets.0.id
+
+output "public_subnets"{
+    value          = aws_subnet.public_subnets.*.id
 }
 
+
+output "private_subnets" {
+    value          = aws_subnet.private_subnets.*.id
+}
+
+
+output "private_subnet_cidrs"{
+    value = aws_subnet.private_subnets.*.cidr_block
+}
+
+
+output "nat_public_ip" {
+	value = aws_eip.nat_public_ip.public_ip 
+}
