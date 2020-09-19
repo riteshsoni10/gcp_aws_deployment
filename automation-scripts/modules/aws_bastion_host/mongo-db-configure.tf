8dca8aad9f98f9eaee60a6e7644b2619e0dba1a0
riteshsoni10 <riteshk.296@gmail.com>
2020-09-19 07:21:08 +0530

    Chnaged variables

--- automation-scripts/modules/aws_bastion_host/mongo-db-configure.tf
+++ automation-scripts/modules/aws_bastion_host/mongo-db-configure.tf
@@ -54,7 +54,7 @@ resource  "null_resource" "mongo_db_configure"{
         inline =[
             "sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y",
 	    "sudo dnf install ansible -y",
-	    "sudo ansible-playbook -u ${var.db_connection_user} -i ${var.db_server_private_ip}, --private-key /tmp/${var.db_instance_key_name} /tmp/db_configuration_scripts/db_server.yml  -e mongo_db_root_username=${var.mongo_db_root_username} -e mongo_db_root_password=${var.mongo_db_root_password} -e mongo_db_server_port=${var.mongo_db_server_port} -e mongo_db_data_path=${var.mongo_db_data_path} -e mongo_db_application_username=${var.mongo_db_application_username} -e mongo_db_application_user_password=${var.mongo_db_application_user_password} -e mongo_db_application_db_name=${var.mongo_db_application_db_name}  --ssh-extra-args=\"-o stricthostkeychecking=no\""
+	    "sudo ansible-playbook -u ${var.db_connection_user} -i ${var.db_server_private_ip}, --private-key /tmp/${var.db_instance_key_name} /tmp/db_configuration_scripts/db_server.yml  -e mongo_db_root_username=${var.mongo_db_root_username} -e mongo_db_root_password=${var.mongo_db_root_password} -e mongo_db_server_port=${var.mongo_db_server_port} -e mongo_db_data_path=${var.mongo_db_data_path} -e application_username=${var.mongo_db_application_username} -e application_user_password=${var.mongo_db_application_user_password} -e application_db_name=${var.mongo_db_application_db_name}  --ssh-extra-args=\"-o stricthostkeychecking=no\""
         ]
     }
 
